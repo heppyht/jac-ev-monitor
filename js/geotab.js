d83330f6455fa531d10eda3ba8993e4d72ef8b1d
heppyht <161272916+heppyht@users.noreply.github.com>
2026-07-07 18:48:01 +0700

    Update geotab.js

--- js/geotab.js
+++ js/geotab.js
@@ -1,48 +1,27 @@
-// ===============================================
-// JAC EV Monitor
-// geotab.js
-// ===============================================
-
 "use strict";
 
-// Global API
 let geotabApi = null;
 
 geotab.addin.jacEvMonitor = function (api, state) {
 
     return {
 
-        // Dipanggil sekali saat Add-In pertama kali dibuka
-        initialize: async function () {
+        initialize: function () {
 
-            console.log("================================");
-            console.log("JAC EV Monitor");
             console.log("Initialize");
-            console.log("================================");
 
             geotabApi = api;
 
         },
 
-        // Dipanggil setiap menu dibuka
         focus: async function () {
 
             console.log("Focus");
 
-            try {
-
-                await loadDashboard();
-
-            }
-            catch (err) {
-
-                console.error(err);
-
-            }
+            await loadDashboard();
 
         },
 
-        // Dipanggil ketika berpindah menu
         blur: function () {
 
             console.log("Blur");
@@ -53,14 +32,7 @@ geotab.addin.jacEvMonitor = function (api, state) {
 
 };
 
-// Helper
 function getApi() {
-
-    if (!geotabApi) {
-
-        throw new Error("MyGeotab API belum tersedia.");
-
-    }
 
     return geotabApi;
 
